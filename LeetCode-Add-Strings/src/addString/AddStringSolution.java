54a93c9505f3e6c0346b870c7ba4f3a5839e1ed7
chaninkou <michaelkoulove@yahoo.com>
2019-06-16 20:26:18 -0700

    Finisheding another one

---
+++ LeetCode-Add-Strings/src/addString/AddStringSolution.java
@@ -0,0 +1,31 @@
+package addString;
+
+public class AddStringSolution {
+	    public String addStrings(String num1, String num2) {
+	        StringBuilder sum = new StringBuilder();
+	        
+	        int carry = 0;
+	        int x;
+	        int y;
+	        
+	        for(int i = num1.length() - 1, j = num2.length() - 1; i >= 0 || j >= 0 || carry == 1; i--, j--){
+	            if(i < 0){
+	                x = 0;
+	            } else {
+	                x = num1.charAt(i) - '0';
+	            }
+	            if(j < 0){
+	                y = 0;
+	            } else {
+	                y = num2.charAt(j) - '0';
+	            }
+	            
+	            sum.append((x + y + carry) % 10);
+	            
+	            carry = (x + y + carry) / 10;
+	            
+	        }
+	        
+	        return sum.reverse().toString();
+	    }
+}
